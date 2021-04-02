0179d15f1895e783b9e394ac9ae90b8d872bb199
XxrrxX <xxrrxx3888@gmail.com>
2021-04-02 02:21:15 -0600

    install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 reset
 echo "Actualizando repositorios..."|lolcat -a -d 50
-apt-get update && sudo apt-get upgrade -s
+pkg update && pkg upgrade 
 clear
 echo "Creando capetas..."|lolcat -a -d 50
 cd ../
 mkdir ubuntu && cd ubuntu/
 clear
 echo "Instalando ubuntu..."|lolcat -a -d 50
-sudo apt-get install wget openssl-tool proot figlet lolcat git -s && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
+pkg install wget openssl-tool proot figlet lolcat git  && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
 clear
 echo "Ubuntu instalado..."|lolcat -a -d 50
 cd ../ && rm -r instalador-ubuntu-termux/ && cd ubuntu/
