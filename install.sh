37b181d7fb4d57cd58bfe6eb01d8408080253837
XxrrxX <xxrrxx3888@gmail.com>
2021-04-02 13:34:18 -0600

    install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ pkg update
 pkg upgrade
 clear
 echo "Creando capetas..."|lolcat -a -d 50
-cd ../
-git clone https//:github.com/mfdgaming/ubuntu-in-termux
+cd ..
+git clone https//:ithub.com/mfdgaming/ubuntu-in-termux
 clear
 echo "Instalando ubuntu..."|lolcat -a -d 50
 pkg install proot
