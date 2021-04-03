faa41c1606ceeb4f6fa4bb71e47f6e1e3855e3f2
XxrrxX <xxrrxx3888@gmail.com>
2021-04-03 00:24:21 -0600

    install.sh

--- install.sh
+++ install.sh
@@ -8,12 +8,14 @@ pkg upgrade
 clear
 echo "Creando capetas..."|lolcat -a -d 50
 cd ..
-git clone https://github.com/
+git clone https://github.com/mfdgaming/ubuntu-in-termux
 clear
 echo "Instalando ubuntu..."|lolcat -a -d 50
-
+cd ubuntu-in-termux/
+chmod 777 ubuntu.sh
 clear
 echo "Ubuntu instalado..."|lolcat -a -d 50
+./ubuntu.sh
 clear
 echo "Ejecutando ubuntu..."|lolcat -a -d 50
-
+./startubuntu.sh
