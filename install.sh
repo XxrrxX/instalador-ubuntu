fa3a10570e64c64e3155fa7490fe0098b2781e76
XxrrxX <xxrrxx3888@gmail.com>
2021-04-03 00:11:13 -0600

    install.sh

--- install.sh
+++ install.sh
@@ -8,14 +8,12 @@ pkg upgrade
 clear
 echo "Creando capetas..."|lolcat -a -d 50
 cd ..
-git clone https://github.com/mfdgaming/ubuntu-in-termux
+git clone https://github.com/
 clear
 echo "Instalando ubuntu..."|lolcat -a -d 50
-pkg install proot
-cd ubuntu-in-termux/
-bash ubuntu.sh
+
 clear
 echo "Ubuntu instalado..."|lolcat -a -d 50
 clear
 echo "Ejecutando ubuntu..."|lolcat -a -d 50
-./startubuntu.sh
+
